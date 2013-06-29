ddbe9829b02f196d9a7b5c56e0e980cd5bbc449f
sakoh <sakohartounian@yahoo.com>
2013-06-28 19:04:39 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,14 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
-   def create
-     @user = User.new(params[:user])
-     if @user.save
-       redirect_to @user
-     else
-       render 'new'
-     end
-   end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,45 +1,16 @@
 require 'spec_helper'
 
-describe "User pages" do
-
-  subject { page }
-
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
-    it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }
-  end
-end
-
 describe User do
+
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
-    password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
-  
+
   subject { @user }
-
-  it { should respond_to(:authenticate) }
-
-  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
-
-  describe "return value of authenticate method" do
+  
+  describe "remember token" do
     before { @user.save }
-    let(:found_user) { User.find_by_email(@user.email) }
-    
-    describe "with valid password" do
-      it { should == found_user.authenticate(@user.password) }
-    end
-
-    describe "with invalid password" do 
-      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-      it { should_not == user_for_invalid_password }
-      specify { user_for_invalid_password.should be_false }
-    end
+    its(:remember_token) { should_not be_blank }
   end
 end
